3aacb538a38fc7a6ecc67fbce7d1323f6d4d26f1
Martin Saposnik <martinrobertosaposnik@gmail.com>
2023-03-23 02:35:53 -0300

    add useRef

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,8 +1,13 @@
+import {useRef} from 'react';
+
 type Props = {image: string}
 
 export const RandomFox = ({ image }: Props): JSX.Element =>{ // forma explicita
 
+    const node = useRef<HTMLImageElement>(null);
+
     return <img 
+    ref={node}
     src ={image}
     width={320}
     height="auto"
